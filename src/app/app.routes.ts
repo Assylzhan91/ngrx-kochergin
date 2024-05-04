95abd4c6c96a683d9b0a17a8f0bf2304415c64eb
Асылжан Баймуратов <asylzanbajmuratov@Noutbuk-Asylzan.local>
2024-05-04 21:05:44 +0500

    lesson-006

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router'
 
-export const routes: Routes = [];
+import {RegisterComponent} from '@auth/register/register.component'
+
+export const routes: Routes = [
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+]

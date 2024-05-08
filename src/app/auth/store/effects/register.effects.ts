12a4c8131e3e3e212ffa1bda253d759fcbccb01b
Асылжан Баймуратов <asylzanbajmuratov@Noutbuk-Asylzan.local>
2024-05-08 18:14:49 +0500

    lesson-017

--- src/app/auth/store/effects/register.effects.ts
+++ src/app/auth/store/effects/register.effects.ts
@@ -1,25 +1,35 @@
 import {Actions, createEffect, ofType} from '@ngrx/effects'
+import {catchError, map, of, switchMap, tap} from 'rxjs'
 import {HttpErrorResponse} from '@angular/common/http'
 import {inject, Injectable} from '@angular/core'
+import {Router} from '@angular/router'
 
+import {AuthErrorResponseInterface} from '@shared/types/auth-error-response.interface'
+import {PersistenceService} from '@shared/services/persistence.service'
 import {CurrentUserInterface} from '@shared/types/user.interface'
 import {AuthService} from '@auth/services/auth.service'
-import {catchError, map, of, switchMap} from 'rxjs'
 import {
   registerAction,
   registerFailureAction,
   registerSuccessAction,
 } from '@auth/store/actions/register.action'
-import {AuthErrorResponseInterface} from '@shared/types/auth-error-response.interface'
 
 @Injectable()
 export class RegisterEffects {
-  register$ = createEffect((actions$ = inject(Actions), authService = inject(AuthService)) => {
-    return actions$.pipe(
+  persistenceService = inject(PersistenceService)
+  authService = inject(AuthService)
+  actions$ = inject(Actions)
+  router = inject(Router)
+
+  register$ = createEffect(() => {
+    return this.actions$.pipe(
       ofType(registerAction),
       switchMap(({request}) => {
-        return authService.register(request).pipe(
-          map((currentUser: CurrentUserInterface) => registerSuccessAction({currentUser})),
+        return this.authService.register(request).pipe(
+          map((currentUser: CurrentUserInterface) => {
+            this.persistenceService.setToken<string>('accessToken', currentUser.token)
+            return registerSuccessAction({currentUser})
+          }),
           catchError((res: HttpErrorResponse) =>
             of(registerFailureAction(res.error as {errors: AuthErrorResponseInterface})),
           ),
@@ -27,4 +37,18 @@ export class RegisterEffects {
       }),
     )
   })
+
+  redirectAfterSubmit$ = createEffect(
+    () => {
+      return this.actions$.pipe(
+        ofType(registerSuccessAction),
+        tap(() => {
+          this.router.navigate(['/home'])
+        }),
+      )
+    },
+    {
+      dispatch: false,
+    },
+  )
 }
